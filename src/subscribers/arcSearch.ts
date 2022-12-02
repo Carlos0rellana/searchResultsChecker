63af9ff1f5cb933a7eaa77c56d833f323decf69e
Felipe <felipe.garcia@publimetro.cl>
2022-12-02 11:44:07 -0300

    Fix findbytitle

--- src/subscribers/arcSearch.ts
+++ src/subscribers/arcSearch.ts
@@ -17,6 +17,8 @@ const searchInArc = async (siteId: string, searchQuery: string, from: string = '
     }
   }
 
+  console.log(`\nhttps://api.metroworldnews.arcpublishing.com/content/v4/search/published?website=${siteId}&q=${searchQuery}\n&_sourceInclude=${returnValues}&from=${from}&size=${size}`);
+
   let query = null;
   let iteraciones = 0;
   while(query == null){
@@ -61,19 +63,15 @@ const reverseSearch = async (siteId: string, search: string, compareOrder: strin
 }
 
 const searchByTitle = async (siteId: string, element: arcSimpleStory): Promise<arcSimpleStory|false> => {
-  const searchQuery = `headlines.basic:${element.title.replace(/:/g, '\\:').replace(/"/g, '\\"')}+AND+type:"story"`
-  const searchConfig: searchInArcItemOptions = {
-    siteId: siteId,
-    search: searchQuery,
-    type: 'url',
-    priority: false
+  console.log('function searchByTitle')
+  const searchQuery = `headlines.basic:"${element.title.replace(/:/g, '\\:').replace(/"/g, '\\"')}"+AND+type:"story"`
+  let data : any = await searchInArc(siteId,searchQuery)
+  if(data !== 'fail'){
+    let result : any = restructureAarcSimpleStory(siteId,data.content_elements[0]);
+    return result;
+
   }
-  const checkByTitle = lookingForASite(searchConfig)
-  if (await checkByTitle !== false) {
-    return await checkByTitle
-  } else {
-    return false
-  }
+  return false
 }
 
 const restructureAarcSimpleStory = (siteId: string, searchResult: any): arcSimpleStory => {
@@ -93,18 +91,23 @@ const restructureAarcSimpleStory = (siteId: string, searchResult: any): arcSimpl
 }
 
 const comparativeResult = (resultList: any, config: ratioElementsOptions, ratio: number = 0.8): [number, arcSimpleStory]|false => {
+  console.log('function comparativeResult')
   let returnValue: [number, arcSimpleStory]|false = false
   if (resultList.content_elements !== undefined) {
+    console.log('Linea 102')
     for (const element of resultList.content_elements) {
       if (element.canonical_url !== undefined) {
+        console.log('Linea 105')
         const ratioValue = ratioWords(element.canonical_url, config)
-
+        console.log('ratioValue',ratioValue);
         if (returnValue === false && ratioValue >= ratio) {
+          console.log('Linea 109')
           const currentUrl = restructureAarcSimpleStory(config.siteId, element)
           returnValue = [ratioValue, currentUrl]
         }
 
         if (returnValue !== false && ratioValue > returnValue[0]) {
+          console.log('Linea 115')
           const currentUrl = restructureAarcSimpleStory(config.siteId, element)
           returnValue = [ratioValue, currentUrl]
         }
@@ -115,26 +118,33 @@ const comparativeResult = (resultList: any, config: ratioElementsOptions, ratio:
 }
 
 const lookingForASite = async (searchConfig: searchInArcItemOptions): Promise <arcSimpleStory|false> => {
+  console.log('function lookingForASite')
   let mainSiteSearch: any = await searchInArc(searchConfig.siteId, searchConfig.search)
   let currentUrl: arcSimpleStory
   console.log('\n<////////////>\ninit search in Arc\n<////////////>\n')
   if (mainSiteSearch === undefined || mainSiteSearch.message === 'fail') {
+    console.log('Line 126')
     mainSiteSearch = await lookingForASite(searchConfig)
   }
   if (mainSiteSearch.count !== undefined && mainSiteSearch.count > 0) {
+    console.log('Line 130')
     const config: ratioElementsOptions = {
       type: searchConfig.type,
       siteId: searchConfig.siteId,
       valueToSearch: searchConfig.search
     }
+    console.log('ratioElementsOptions',config)
     const checkingItem = comparativeResult(mainSiteSearch, config)
-    if (checkingItem !== false) {
+    console.log('checkingItem',checkingItem)
+    if ((checkingItem !== false && checkingItem.length > 0) || (checkingItem !== false && searchConfig.search.match('headlines.basic') !== null)) {
+      console.log('Line 138')
       currentUrl = checkingItem[1]
       console.log(currentUrl)
       console.log('\n<////////////>\nend search in Arc\n<////////////>\n')
       return currentUrl
     }
   }
+  console.log('Line 145')
   console.log(false)
   console.log('\n<////////////>\nend search in Arc\n<////////////>\n')
   return false
@@ -193,6 +203,7 @@ export const searchInBucleArc = async (siteId: string, search: string, currentPr
       priority: currentPriority
     }
     find = await lookingForASite(searchConfig)
+    console.log('Linea 211',find);
     if (await find === false) {
       find = await bucleSeachInSitesList(siteId, search, currentPriority)
     }
@@ -213,8 +224,9 @@ export const searchInBucleArc = async (siteId: string, search: string, currentPr
   //   }
   // }
   if (find !== false && currentPriority === false && (await find?.type === 'gallery' || await find?.type === 'video')) {
+    console.log('Gallery Found');
     const checkByTitle = await searchByTitle(siteId, find)
-    if (await checkByTitle !== false) {
+    if (checkByTitle !== false) {
       return checkByTitle
     } else {
       const returnValue = await bucleSeachByTitleInSitesList(siteId, find)
